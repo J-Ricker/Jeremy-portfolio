5ab4fb07c91652eb40b764eec661692544f7fc70
J-Ricker <lvlgrinddesign@gmail.com>
2019-03-24 17:53:27 -0400

    fixed copy email button

--- js/main.js
+++ js/main.js
@@ -1,54 +1,3 @@
-var ctx = document.getElementById('myChart');
-var myBarChart = new Chart(ctx, {
-    type: 'horizontalBar',
-    data: {
-        labels: ['HTML', 'CSS', 'JAVASCRIPT', 'NODE.JS', 'REACT'],
-        datasets: [{
-            label: '',
-            data: [90, 80, 80, 55, 50, 100, 1],
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-            ],
-
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                barPercentage: 0.4,
-                ticks: {
-                    beginAtZero: true,
-                    fontSize: 15,
-                    fontStyle: 'bold',
-                    fontColor: '#000',
-                },
-                gridLines: {
-                    drawOnChartArea: false,
-                    drawBorder: false,
-                    
-                }
-            }],
-            xAxes: [{
-                gridLines: {
-                    drawOnChartArea: false,
-                    drawBorder: false
-                },
-                ticks: {
-                    display: true
-                }
-            }]
-        },
-        legend: {
-            display: false,
-        }
-    }
-});
-
-
 var copyEmailBtn = document.querySelector('.js-emailcopybtn');  
 copyEmailBtn.addEventListener('click', function(event) {  
     // Select the email link anchor text  
@@ -68,5 +17,5 @@ copyEmailBtn.addEventListener('click', function(event) {
 
     // Remove the selections - NOTE: Should use
     // removeRange(range) when it is supported  
-    window.getSelection().removeAllRanges();  
+    // window.getSelection().removeAllRanges();  
 });
